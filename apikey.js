24b51a26fe61cc6059c8fca1fbb9322b438321dd
jwjunjun22 <ljw404103@gmail.com>
2026-05-20 15:43:47 +0900

    파일 두개 삭제

--- apikey.js
+++
@@ -1,5 +0,0 @@
-const KEY_PART1 = "AIzaSyBdIXOfhBD5EsH4H6T"; // 키 앞부분 적기
-const KEY_PART2 = "7C3M46iNpfL40Bi4";   // 키 뒷부분 적기
-
-// index.html에서 불러와서 쓸 수 있도록 전역 변수에 결합
-window.GEMINI_API_KEY = KEY_PART1 + KEY_PART2;
