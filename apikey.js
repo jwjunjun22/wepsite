e5639c7f6c186c54554d547fe3a1addc960c7233
jwjunjun22 <ljw404103@gmail.com>
2026-05-20 15:04:18 +0900

    Update apikey.js

--- apikey.js
+++ apikey.js
@@ -1,5 +1,5 @@
-const KEY_PART1 = "AIzaSyBP1oA-PN6qdg43K"; // 키 앞부분 적기
-const KEY_PART2 = "y66DM9sT9QeqkhgP1o";   // 키 뒷부분 적기
+const KEY_PART1 = "AIzaSyBdIXOfhBD5EsH4H6T"; // 키 앞부분 적기
+const KEY_PART2 = "7C3M46iNpfL40Bi4";   // 키 뒷부분 적기
 
 // index.html에서 불러와서 쓸 수 있도록 전역 변수에 결합
 window.GEMINI_API_KEY = KEY_PART1 + KEY_PART2;
